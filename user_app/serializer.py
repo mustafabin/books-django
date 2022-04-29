4d85b4c8e48ee0fc044ffb0e5ab6eff625aa7bc6
mustafabin <mbin828@gmail.com>
2022-04-29 09:45:56 -0400

    fixed fields

--- user_app/serializer.py
+++ user_app/serializer.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id','username','last_login']
